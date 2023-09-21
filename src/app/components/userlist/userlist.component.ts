79edf36f84f74fde40fa5b6dbf847a42086657cb
thakkarjanvi <janvithakkar18122001@gmail.com>
2023-09-21 14:11:13 +0530

    Edited

--- src/app/components/userlist/userlist.component.ts
+++ src/app/components/userlist/userlist.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/services/user.service';
 import { User } from 'src/app/models/user.model';
-
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-userlist',
@@ -11,15 +11,17 @@ import { User } from 'src/app/models/user.model';
 export class UserlistComponent implements OnInit{
   users: User[] = [];
 
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService, private toastr: ToastrService,) {}
 
   ngOnInit(): void {
     this.userService.getUsers().subscribe((data:any) => {
       this.users = data.users;
+      this.toastr.success('User list fetched successfully!', 'Success');
       console.log(data);
     },
     (error) => {
       console.log('Error fetching user list:', error);
+      this.toastr.error('Error fetching user list', 'Error');
   }
   );
 }
