ec33abbc929ff1661ed181138c20ba423b8343c7
thakkarjanvi <janvithakkar18122001@gmail.com>
2023-10-13 11:51:29 +0530

    Implemented Social Login Successfully

--- src/app/components/registration/registration.component.ts
+++ src/app/components/registration/registration.component.ts
@@ -35,52 +35,53 @@ export class RegistrationComponent implements OnInit {
      
 
      
-    //  window.onGoogleLibraryLoad = () => {
-    //   //@ts-ignore
-    //   google.accounts.id.initialize({
-    //     client_id: '93789025494-p83u7qfgrlqdc7kv4knm924knd83vlok.apps.googleusercontent.com',
-    //     callback: this.credentialResponse.bind(this),
-    //     auto_select: false,
-    //     cancel_on_tap_outside: true,
-    //   });
-    //   // @ts-ignore
-    //   google.accounts.id.renderButton(
-    //     // @ts-ignore
-    //     document.getElementById('google-button'),
-    //     { theme: 'outline', size: 'large', width: '100%' }
-    //   );
-    //   // @ts-ignore
-    //   google.accounts.id.prompt((notification: PromptMomentNotification) => {});
-    // };
-
-
-
-
-    const script = document.createElement('script');
-    script.src = 'https://accounts.google.com/gsi/client';
-    script.onload = () => {
-      // Initialize and render the Google Sign-In button after the script has loaded
-      // @ts-ignore
+     window.onGoogleLibraryLoad = () => {
+      //@ts-ignore
       google.accounts.id.initialize({
-        client_id: '93789025494-p83u7qfgrlqdc7kv4knm924knd83vlok.apps.googleusercontent.com',
+        client_id: '131259041588-7lrt26av8rp1nm0tf3m6vq0bpaj3b23g.apps.googleusercontent.com',
         callback: this.credentialResponse.bind(this),
         auto_select: false,
         cancel_on_tap_outside: true,
       });
-  
       // @ts-ignore
       google.accounts.id.renderButton(
         // @ts-ignore
         document.getElementById('google-button'),
         { theme: 'outline', size: 'large', width: '100%' }
       );
-  
       // @ts-ignore
       google.accounts.id.prompt((notification: PromptMomentNotification) => {});
     };
+  }
+
+
+
+
+  //   const script = document.createElement('script');
+  //   script.src = 'https://accounts.google.com/gsi/client';
+  //   script.onload = () => {
+  //     // Initialize and render the Google Sign-In button after the script has loaded
+  //     // @ts-ignore
+  //     google.accounts.id.initialize({
+  //       client_id: '131259041588-7lrt26av8rp1nm0tf3m6vq0bpaj3b23g.apps.googleusercontent.com',
+  //       callback: this.credentialResponse.bind(this),
+  //       auto_select: false,
+  //       cancel_on_tap_outside: true,
+  //     });
   
-    // Append the script to the document to load the Google Identity Services library
-    document.head.appendChild(script);
+  //     // @ts-ignore
+  //     google.accounts.id.renderButton(
+  //       // @ts-ignore
+  //       document.getElementById('google-button'),
+  //       { theme: 'outline', size: 'large', width: '100%' }
+  //     );
+  
+  //     // @ts-ignore
+  //     google.accounts.id.prompt((notification: PromptMomentNotification) => {});
+  //   };
+  
+  //   // Append the script to the document to load the Google Identity Services library
+  //   document.head.appendChild(script);
 
 
 
@@ -89,27 +90,31 @@ export class RegistrationComponent implements OnInit {
 
 
     
-  }
+  // }
 
   private credentialResponse(response: CredentialResponse) {
     this.authService
       .LoginWithGoogle(response.credential)
-      .subscribe((x: any) => {
-        this.zone.run(() => {
-          if (x.statusCode === 200) {
+      .subscribe(
+        (x: any) => {
+          console.log("a", x);
+          this.zone.run(() => {
             // Registration successful
-        this.toastr.success('Google Registration successful!', 'Success');
-        // Redirect to login page or perform other actions
-        this.router.navigate(['/login']);
-
-            //this.router.navigateByUrl('/login');
-          } else {
-            this.errorMessage = 'Google Registration failed. Please try again.';
-            this.toastr.error(this.errorMessage, 'Error');
-          }
-        });
-      });
+            this.toastr.success('Google Registration successful!', 'Success');
+            // Redirect to login page or perform other actions
+            this.router.navigate(['/login']);
+          });
+        },
+        (error: any) => {
+          console.log(error);
+          // Handle login error if needed
+        }
+      );
   }
+  
+  
+
+
 
   onSubmit() {
     if (this.registerForm.valid) {
@@ -130,5 +135,6 @@ export class RegistrationComponent implements OnInit {
     );
   }
 
-  }
 }
+  }
+
