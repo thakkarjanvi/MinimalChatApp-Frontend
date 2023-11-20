5d9566ef99174cb617d123c9f6210a1d0ffd5f09
thakkarjanvi <janvithakkar18122001@gmail.com>
2023-11-20 09:33:57 +0530

    Updated

--- src/app/components/add-members-box/add-members-box.component.ts
+++ src/app/components/add-members-box/add-members-box.component.ts
@@ -1,46 +1,46 @@
-// import { Component, Inject } from '@angular/core';
-// import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-// import { User } from 'src/app/models/user.model';
-// import { UserService } from 'src/app/services/user.service';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { User } from 'src/app/models/user.model';
+import { UserService } from 'src/app/services/user.service';
 
 
-// @Component({
-//   selector: 'app-add-members-box',
-//   templateUrl: './add-members-box.component.html',
-//   styleUrls: ['./add-members-box.component.css']
-// })
-// export class AddMembersBoxComponent {
-//   allusers: any[] = [];
-//   constructor(
-//     private userService: UserService,
-//     public dialogRef: MatDialogRef<AddMembersBoxComponent>,
-//     @Inject(MAT_DIALOG_DATA) public data: any
-//   ) {
-//     this.userService.getUsers(true).subscribe((users: any) => {
-//       this.allusers = users.data.map((user: User) => ({
-//         ...user,
-//         isSelected: false,
-//       }));
-//     });
-//   }
-//   groupUsers = this.data.groupUsers;
+@Component({
+  selector: 'app-add-members-box',
+  templateUrl: './add-members-box.component.html',
+  styleUrls: ['./add-members-box.component.css']
+})
+export class AddMembersBoxComponent {
+  allusers: any[] = [];
+  constructor(
+    private userService: UserService,
+    public dialogRef: MatDialogRef<AddMembersBoxComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {
+    this.userService.getUsers(true).subscribe((users: any) => {
+      this.allusers = users.data.map((user: User) => ({
+        ...user,
+        isSelected: false,
+      }));
+    });
+  }
+  groupUsers = this.data.groupUsers;
 
-//   availableUsers = this.data.groupUsers;
-//   isUserInGroup(user: User): boolean {
-//     const isUserInGroup = this.groupUsers.some(
-//       (groupUser: any) => groupUser.userId === user.id
-//     );
-//     return !isUserInGroup;
-//   }
+  availableUsers = this.data.groupUsers;
+  isUserInGroup(user: User): boolean {
+    const isUserInGroup = this.groupUsers.some(
+      (groupUser: any) => groupUser.userId === user.id
+    );
+    return !isUserInGroup;
+  }
 
-//   onSubmit() {
-//     // Collect the selected users
-//     const selectedUsers = this.allusers.filter((user) => user.isSelected);
-//     // Close the dialog
-//     this.dialogRef.close(selectedUsers);
-//   }
+  onSubmit() {
+    // Collect the selected users
+    const selectedUsers = this.allusers.filter((user) => user.isSelected);
+    // Close the dialog
+    this.dialogRef.close(selectedUsers);
+  }
 
-//   onClose() {
-//     this.dialogRef.close();
-//   }
-// }
+  onClose() {
+    this.dialogRef.close();
+  }
+}
